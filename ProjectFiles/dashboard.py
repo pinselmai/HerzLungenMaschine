ab7f6f82d2957592dcf55be7015865dcf1462e79
Hannes <test.user@xyz.com>
2022-05-23 09:47:24 +0200

    3

--- ProjectFiles/dashboard.py
+++ ProjectFiles/dashboard.py
@@ -190,17 +190,17 @@ def bloodflow_figure(value, bloodflow_checkmarks):
 
     avg = bf.mean() #Mittelwert berechnen
     x = [0, 480] #Grenzen für x-Werte
-    y = [avg.loc['Blood Flow (ml/s)']]
+    y = avg.loc['Blood Flow (ml/s)']
     fig3.add_trace(go.Scatter(x = x , y = [y, y], mode = 'lines', name = 'Mittelwert', line_color = 'seagreen'))
 
     #Intervalle um Mittelwert: 3.2
 
     #Obere Grenze
-    y_oben = [avg.loc['Blood Flow (ml/s)']]*1.15 #115% vom Mittelwert
+    y_oben = avg.loc['Blood Flow (ml/s)']*1.15 #115% vom Mittelwert
     fig3.add_trace(go.Scatter(x = x, y = [y_oben,y_oben],mode = 'lines', line_color = 'red', name = 'Obere Grenze'))
 
     #Untere Grenze
-    y_unten = [avg.loc['Blood Flow (ml/s)']]*0.85 #85% vom Mittelwert
+    y_unten = avg.loc['Blood Flow (ml/s)']*0.85 #85% vom Mittelwert
     fig3.add_trace(go.Scatter(x = x, y = [y_unten,y_unten],mode = 'lines', line_color = 'red', name = 'Untere Grenze'))
 
     #3.3
