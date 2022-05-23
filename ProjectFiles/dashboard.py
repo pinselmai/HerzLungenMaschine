78456db9c2b89cd0b44730f214c15eb8ba96229b
Hannes <test.user@xyz.com>
2022-05-23 11:18:56 +0200

    layout colors

--- ProjectFiles/dashboard.py
+++ ProjectFiles/dashboard.py
@@ -137,7 +137,9 @@ def update_figure(value, algorithm_checkmarks):
     # Blood Temperature
     fig2 = px.line(ts, x="Time (s)", y = data_names[2])
 
-    fig0.update_layout(plot_bgcolor = colors['background'], paper_bgcolor = colors['background'], font_color = colors['text'], gridcolor ='text', linecolor='text')
+    fig0.update_layout(plot_bgcolor = colors['background'], paper_bgcolor = colors['background'],
+                       font_color = colors['text'], gridcolor = colors ['text'], linecolor = colors ['text'])
+
     fig1.update_layout(plot_bgcolor = colors['background'], paper_bgcolor = colors['background'], font_color = colors['text'])
     fig2.update_layout(plot_bgcolor = colors['background'], paper_bgcolor = colors['background'], font_color = colors['text'])
 
