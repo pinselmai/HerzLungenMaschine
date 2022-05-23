17f7dfe57f80a91c12b28368e783372b3da6e315
Hannes <test.user@xyz.com>
2022-05-23 13:39:56 +0200

    n=40

--- ProjectFiles/dashboard.py
+++ ProjectFiles/dashboard.py
@@ -191,6 +191,7 @@ def update_figure(value, algorithm_checkmarks):
 def bloodflow_figure(value, bloodflow_checkmarks):
     
     ## Calculate Moving Average: Aufgabe 2
+
     print(bloodflow_checkmarks)
     bf = list_of_subjects[int(value)-1].subject_data
 
@@ -217,7 +218,7 @@ def bloodflow_figure(value, bloodflow_checkmarks):
 
     #Simple Moving Average (SMA)
         if bloodflow_checkmarks == ["SMA"]:
-            bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf["Blood Flow (ml/s)"],4) 
+            bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf["Blood Flow (ml/s)"],40) 
             fig3.add_trace(go.Scatter(x=bf["Time (s)"],y=bf["Blood Flow (ml/s) - SMA"],mode='lines', marker_color = 'fuchsia', name= 'SMA'))
 
 
