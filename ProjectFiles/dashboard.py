ed8f832bdea9a216e1469c6d2842f65e87278e07
Hannes <test.user@xyz.com>
2022-05-25 11:08:45 +0200

    testfix

--- ProjectFiles/dashboard.py
+++ ProjectFiles/dashboard.py
@@ -219,20 +219,20 @@ def bloodflow_figure(value, bloodflow_checkmarks):
     #Cumulative Moving Average (CMA)
     if bloodflow_checkmarks is not None: 
 
-        if 'CMA' in bloodflow_checkmarks:
+        if "CMA" in bloodflow_checkmarks:
             bf["Blood Flow (ml/s) - CMA"] = ut.calculate_CMA(bf["Blood Flow (ml/s)"],4) 
             fig3.add_trace(go.Scatter(x=bf["Time (s)"],y=bf["Blood Flow (ml/s) - CMA"],mode='lines', marker_color = 'yellow', name= 'CMA'))
 
 
     #Simple Moving Average (SMA)
-        if 'SMA' in bloodflow_checkmarks:
+        if "SMA" in bloodflow_checkmarks:
             bf["Blood Flow (ml/s) - SMA"] = ut.calculate_SMA(bf["Blood Flow (ml/s)"],4) 
             fig3.add_trace(go.Scatter(x=bf["Time (s)"],y=bf["Blood Flow (ml/s) - SMA"],mode='lines', marker_color = 'fuchsia', name= 'SMA'))
 
 
 
     #Blood Flow Alarm: Aufgabe 3
-        if 'Show Limits' in bloodflow_checkmarks:
+        if "Show Limits" in bloodflow_checkmarks:
 
     #Intervalle um Mittelwert: 3.2
 
